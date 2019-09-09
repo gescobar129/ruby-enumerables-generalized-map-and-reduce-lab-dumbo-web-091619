d08de770388ba59d1f69b26b3c7c863cb70ed336
Gaida M Escobar <gescobar129@gmail.com>
2019-09-09 16:51:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,9 +4,9 @@ def my_own_map(array)
   i = 0 
   while i < array.length do
     new_array.push(yield(array[i]))
-    
-  array.map(block)
-
+    i += 1 
+  end
+  new_array
 end
 
-my_own_map{ |n| n * -1 }
+
