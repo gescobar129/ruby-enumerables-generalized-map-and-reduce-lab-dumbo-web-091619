0fa70e0b26cd7f8e330db55774c5bf11715ed0d8
Gaida M Escobar <gescobar129@gmail.com>
2019-09-09 17:07:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,7 @@ def map(array)
 end
 
 def reduce(array, sp=nil)
-  #sp = starting point. we make second argument optional by giving it  a default value of nil.
+  #sp = starting point. we make second argument optional by giving it  a default value of nil.that means tht
   if sp
     sum = sp
     i = 0 
