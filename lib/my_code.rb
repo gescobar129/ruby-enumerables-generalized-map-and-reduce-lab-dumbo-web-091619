b517ca8880657f036fc00992e0539fee8fd98a7d
Gaida M Escobar <gescobar129@gmail.com>
2019-09-09 16:58:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,12 +1,12 @@
 # Your Code Here
-def my_own_map(array)
+def map(array)
   new_array = []
   i = 0 
   while i < array.length 
     new_array.push(yield(array[i]))
     i += 1 
-  end
-  new_array
+    end
+    new_array 
 end
 
 
